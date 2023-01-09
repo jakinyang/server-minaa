144e9490f9e2c6b9f1c9826d3d7c90415a00f7b3
jakinyang <109392405+jakinyang@users.noreply.github.com>
2023-01-09 08:04:32 -0800

    --refactor resolvers page to import refactored components

--- src/resolvers.js
+++ src/resolvers.js
@@ -1,66 +1,13 @@
-import { GraphQLScalarType, Kind } from 'graphql';
-
-const DateTime = new GraphQLScalarType({
-  name: 'DateTime',
-  description: 'Date custom scalar type',
-  serialize(value) {
-    const date = new Date(value)
-
-    return date.toISOString();
-  },
-  parseValue(value) {
-    return new Date(value);
-  },
-  parseLiteral(ast) {
-    if (ast.kind === Kind.INT) {
-      return parseInt(ast.value, 10);
-    }
-    return null;
-  },
-});
+import * as Query from './resolvers/Query.js';
+import * as User from './resolvers/User.js';
+import * as Report from './resolvers/Report.js';
+import * as Mutation from './resolvers/Mutation.js';
+import { DateTime } from './resolvers/DateTime.js';
 
 export const resolvers = {
-  Query: {
-    test: () => "Hello World",
-    users: function (parent, args, context, info) {
-      return context.prisma.user.findMany();
-    },
-    reports: function (parent, args, context, info) {
-      return context.prisma.report.findMany();
-    },
-    user: function (parent, args, context) {
-
-      return context.prisma.user.findUnique({
-        where: {
-          id: +args.id
-        }
-      });
-    },
-    report: function (parent, args, context) {
-      return context.prisma.report.findUnique({
-        where: {
-          id: +args.id
-        }
-      });
-    },
-  },
-  User: {
-    reports: function (parent, args, context) {
-      return context.prisma.report.findMany({
-        where: {
-          userId: +parent.id
-        }
-      });
-    }
-  },
-  Report: {
-    user: function (parent, args, context) {
-      return context.prisma.user.findUnique({
-        where: {
-          id: +parent.userId
-        }
-      });
-    }
-  },
-  DateTime: DateTime,
+  Query,
+  Mutation,
+  User,
+  Report,
+  DateTime,
 }
